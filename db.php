fb10d5f0d43b4f504b196b446eb8fa5b7ad2f544
Enrique Pinedo <enrjon@gmail.com>
2019-08-31 16:16:07 -0400

    modded db

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $dbname = 'learnsmart';
 $user = 'postgres';
-$pass = 'Enrjon12';
+$pass = 'password';
 $port = 1900;
 
 $dbconn = pg_connect("host='$host' port='$port' dbname='$dbname' user='$user' password='$pass'") or die('Could not connect: ' .pg_last_error());
--- header.php
+++ header.php
@@ -6,7 +6,6 @@
   <meta name = viewport content="width=device-width, initial-scale=1">
   <title></title>
   <link rel="stylesheet" href="css/style.css">
-  <?php //include 'css/css.html';?>
 </head>
   <header>
       <nav class = "nav-content">
@@ -19,61 +18,8 @@
         </ul>
         <div class="nav-right">
             <div class="tab-content">
-                <div class="tab" id="login">
-                    <h3 class="required-header">* - Required</h3>
-                    <form action="includes/login.php" method = "post">
-                        <div class="field-wrap">
-                            <label>
-                                <span class="required-header">*</span>Email Address
-                            </label>
-                            <input type="email" required autocomplete="off" name="email"/>
-                        </div>
-                        <div class="field-wrap">
-                            <label>
-                                <span class="required-header">*</span>Password
-                            </label>
-                            <input type="password" required autocomplete="off" name="password"/>
-                        </div>
-                        <p class="forgot">
-                            <a href="forgot.php">
-                                Forgot Password
-                            </a>
-                        </p>
-                        <button class="button button-block" name="login"/>Log In</button>
-                    </form>
-                </div>
-
-                <div class="tab" id="register">
-                    <h3 class="required-header">* - Required</h3>
-                    <form action="includes/register.php" method = "post">
-                        <div class="field-wrap">
-                            <label>
-                                Name<span class="required-header"></span>
-                            </label>
-                            <input type="text" required autocomplete="on" name="name"/>
-                        </div>
-                        <div  class="field-wrap">
-                            <label>
-                                Email Address<span class="required-header"></span>
-                            </label>
-                            <input type="email" required autocomplete="on" name="email"/>
-                        </div>
-                        <div class="field-wrap">
-                            <label>
-                                Choose A Password
-                            </label>
-                            <input type="password" required autocomplete="off" name="password"/>
-                        </div>
-
-                        <button class="button button-block" name="register"/>Register</button>
-                    </form>
-
-                </div>
-                <ul>
-                    <li class="tab active"><a href="#register">Register</a></li>
-                    <li class="tab"><a href="#login">Log In</a></li>
-                </ul>
-            </div>            
+                <?php require 'navTabs.html'?>
+            </div>
         </div>
       </nav>
 
