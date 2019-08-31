33fbf66239c7e8d3fb073b225fd6f5e7c135c190
Enrique Pinedo <enrjon@gmail.com>
2019-08-31 19:48:56 -0400

    update

--- header.php
+++ header.php
@@ -9,22 +9,19 @@
 </head>
   <header>
       <nav>
-          <div class="nav-content">
-              <a><img src = "img/logo.png" alt = "logo"></a>
-              <ul id="site-nav">
-                <li><a href = "index.php">Home</a></li>
-                <li><a href = "">Nav Tab 1</a></li>
-                <li><a href = "">Nav Tab 2</a></li>
-                <li><a href = "">Nav Tab 3</a></li>
-              </ul>
-          </div>
-          <div class="nav-content">
+          <ul class="content">
+            <li><a href="index.php"><img src = "img/logo.png" alt = "logo" width="35%"></a></li>
+            <li><a href = "index.php">Home</a></li>
+            <li><a href = "">Nav Tab 1</a></li>
+            <li><a href = "">Nav Tab 2</a></li>
+            <li><a href = "">Nav Tab 3</a></li>
+          </ul>
+          <div class="content">
             <div class="tab-content">
                 <?php //require 'navTabs.html'?>
             </div>
         </div>
       </nav>
-
       <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'>
       </script>
       <script src="js/index.js"></script>
