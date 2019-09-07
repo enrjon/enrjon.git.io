f8a0a3f4261bacacee1604b3ed77e968033c29fd
Enrique Pinedo <enrjon@gmail.com>
2019-09-06 21:47:48 -0400

    icon animations

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const hamburger = document.querySelector('.icon');
+const icon = document.querySelector('.icon');
 const navLeft = document.querySelector('.nav-left');
+const navTabs = document.querySelectorAll('.nav-tab a');
+
 const navFolder = document.querySelector('.folder');
 const navForms = document.querySelectorAll('.hforms');
-const navTabs = document.querySelectorAll('.nav-left li');
 
-if(hamburger){
-    hamburger.addEventListener("click", () => {
+if(icon){
+    icon.addEventListener("click", () => {
         if(navLeft){
             navLeft.classList.toggle("open");
+            var i;
+            for (i = 0; i < navTabs.length; i++) {
+              navTabs[i].classList.toggle("show");
+            }
+
         } else if(navFolder){
             navFolder.classList.toggle("open");
         }
