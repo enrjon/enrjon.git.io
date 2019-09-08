04d4808520a892124c2fa233bd58bb7e21adbc8c
Enrique Pinedo <enrjon@gmail.com>
2019-09-07 22:41:37 -0400

    testing nav form update

--- app.js
+++ app.js
@@ -13,10 +13,27 @@ if(icon){
             for (i = 0; i < navTabs.length; i++) {
               navTabs[i].classList.toggle("show");
             }
-
         } else if(navFolder){
             navFolder.classList.toggle("open");
         }
 
     });
 }
+
+const hforms = document.querySelector('.tabs');
+
+hforms.addEventListener("click", () => {
+    var i, tabcontent, tablinks;
+    tabcontent = document.querySelectorAll(".hforms form");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        console.log(tabcontent[i].id);
+    }
+    tablinks = document.getElementsByClassName("tab");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabcontent[1].id).style.display = "block";
+    event.currentTarget.className += " active";
+
+});
