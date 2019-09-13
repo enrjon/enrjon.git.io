cdab1c02c47073f807d56b27a9251e6c40a79dd2
Enrique Pinedo <enrjon@gmail.com>
2019-09-12 23:17:22 -0400

    added resume data; created base folder widget

--- app.js
+++ app.js
@@ -9,28 +9,3 @@ icon.addEventListener("click", () => {
       navTabs[i].classList.toggle("show");
     }
 });
-/*
-navFolder.addEventListener("click", () => {
-    navForms.classList.toggle("open");
-    var i;
-    for (i = 0; i < navForms.length; i++) {
-      navForms[i].classList.toggle("show");
-    }
-});
-// navigation form switching
-
-const hforms = document.querySelector('.tabs');
-var i=0, x , tabcontent, tablinks;
-tabcontent = document.querySelectorAll(".hforms form");
-tablinks = document.getElementsByClassName("tab");
-
-tablinks[0].addEventListener("click", () => {
-    console.log(tablinks[0].classList);
-    tabcontent[0].style.display = "initial";
-    tabcontent[1].style.display = "none";
-});
-tablinks[1].addEventListener("click", () => {
-    console.log(tablinks[1].classList);
-    tabcontent[0].style.display = "none";
-    tabcontent[1].style.display = "initial";
-});*/
---
+++ js/resume.js
@@ -0,0 +1,32 @@
+const projTabs = document.querySelectorAll('.folder ul a');
+const ss = document.querySelectorAll('.folder ul a')[0];
+const qb = document.querySelectorAll('.folder ul a')[1];
+const kg = document.querySelectorAll('.folder ul a')[2];
+const np = document.querySelectorAll('.folder ul a')[3];
+
+var showToggle = function(event){
+    for(i of projTabs){
+        var hide = document.getElementById(i.text);
+        hide.classList.remove('active');
+    }
+}
+ss.addEventListener("click", () => {
+    showToggle();
+    var show = document.getElementById(ss.text);
+    show.classList.toggle('active');
+});
+qb.addEventListener("click", () => {
+    showToggle();
+    var show = document.getElementById(qb.text);
+    show.classList.toggle('active');
+});
+kg.addEventListener("click", () => {
+    showToggle();
+    var show = document.getElementById(kg.text);
+    show.classList.toggle('active');
+});
+np.addEventListener("click", () => {
+    showToggle();
+    var show = document.getElementById(np.text);
+    show.classList.toggle('active');
+});
