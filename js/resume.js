9baaf98e6eeb6cfe9b5a33b197b0d12fa721c55a
Enrique Pinedo <enrjon@gmail.com>
2019-09-14 17:03:20 -0400

    color/background update; spacing changes

--- js/resume.js
+++ js/resume.js
@@ -6,6 +6,8 @@ const np = document.querySelectorAll('.folder ul a')[3];
 
 var showToggle = function(event){
     for(i of projTabs){
+
+        i.classList.remove('active');
         var hide = document.getElementById(i.text);
         hide.classList.remove('active');
     }
@@ -14,19 +16,23 @@ ss.addEventListener("click", () => {
     showToggle();
     var show = document.getElementById(ss.text);
     show.classList.toggle('active');
+    document.querySelectorAll('.folder ul a')[0].classList.toggle('active');
 });
 qb.addEventListener("click", () => {
     showToggle();
     var show = document.getElementById(qb.text);
     show.classList.toggle('active');
+    document.querySelectorAll('.folder ul a')[1].classList.toggle('active');
 });
 kg.addEventListener("click", () => {
     showToggle();
     var show = document.getElementById(kg.text);
     show.classList.toggle('active');
+    document.querySelectorAll('.folder ul a')[2].classList.toggle('active');
 });
 np.addEventListener("click", () => {
     showToggle();
     var show = document.getElementById(np.text);
     show.classList.toggle('active');
+    document.querySelectorAll('.folder ul a')[3].classList.toggle('active');
 });
